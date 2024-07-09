7fa1d2375249bbf5a944d060e4f63e9fd8c27f20
Tom Dobson <dobson980@gmail.com>
2024-07-08 23:07:43 -0700

    concept app complete

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
+    @State private var toggleIsOn: Bool = true
+    @State private var name: String = ""
+    @State private var color: Color = .orange
+    @State private var date: Date = Date()
+    @State private var steps: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Group {
+                Spacer()
+                
+                Text("Press Count: \(count)")
+                    .padding()
+                Button("Press Me") {
+                    count += 1
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                TextField("Enter a name", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                Text("Name entered: \(name)")
+                
+                Spacer()
+                
+                Toggle("Toggle is \(toggleIsOn ? "on" : "off"):", isOn: $toggleIsOn)
+                    .padding()
+                
+                Spacer()
+                
+            }
+            
+            Rectangle()
+                .fill(color)
+                .frame(width: 100, height: 100)
+                .padding()
+            ColorPicker("Pick a rectangle color:", selection: $color)
+            
+            Spacer()
+            
+            DatePicker("Date: ", selection: $date)
+            Text("Selected Date: \(date.formatted(date: .abbreviated, time: .shortened))")
+            
+            Spacer()
+            
+            Stepper("Step Count: \(steps)", value: $steps, in: 0...10)
+            
+            Spacer()
         }
+        .font(.title2)
         .padding()
     }
 }
